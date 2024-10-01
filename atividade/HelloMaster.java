0bb3150f8d7040b0325f3b6745cf415128f36721
Arthur <arthurborba271@gmail.com>
2024-10-01 09:55:22 -0300

    a

--- atividade/HelloMaster.java
+++ atividade/HelloMaster.java
@@ -6,6 +6,15 @@ public class HelloMaster{
     public static void letsGetSomeHellos(){
         Thread t1 = new Thread(new HelloFromThread()); 
         t1.start();
+
+        Thread t2 = new Thread(new HelloFromThread()); 
+        t2.start();
+
+        Thread t3 = new Thread(new HelloFromThread()); 
+        t3.start();
+
+        Thread d4 = new Thread(new HelloFromThread()); 
+        d4.start();
         
     }
 }
