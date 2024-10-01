b32446118a70ec724f7c144eda59fc8f5561711f
Arthur <arthurborba271@gmail.com>
2024-10-01 09:44:51 -0300

    a

--- atividade/HelloMaster.java
+++ atividade/HelloMaster.java
@@ -5,7 +5,7 @@ import modelo.HelloFromThread;
 public class HelloMaster{
     public static void letsGetSomeHellos(){
         Thread t1 = new Thread(new HelloFromThread()); 
-        t1.run();
+        t1.start();
         
     }
 }
