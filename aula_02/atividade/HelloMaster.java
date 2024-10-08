e295b6224ae4163b085e994fbb5663cf83599856
borbinha27 <141046101+borbinha27@users.noreply.github.com>
2024-10-08 12:07:21 +0000

    a

--- aula_02/atividade/HelloMaster.java
+++ aula_02/atividade/HelloMaster.java
@@ -4,17 +4,10 @@ import aula_02.modelo.HelloFromThread;
 
 public class HelloMaster{
     public static void letsGetSomeHellos(){
-        Thread t1 = new Thread(new HelloFromThread(1)); 
-        t1.start();
-
-        Thread t2 = new Thread(new HelloFromThread(2)); 
-        t2.start();
-
-        Thread t3 = new Thread(new HelloFromThread(3)); 
-        t3.start();
-
-        Thread d4 = new Thread(new HelloFromThread(4)); 
-        d4.start();
+        for (int i = 0; i < 4; i++) {
+            Thread t1 = new Thread(new HelloFromThread(i)); 
+            t1.start();
+        }
         
     }
 }
